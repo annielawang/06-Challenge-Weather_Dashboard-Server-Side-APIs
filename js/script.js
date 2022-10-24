2af38face62a64903ebffed9759bf1ab8813587a
Annie Li <annielawang@gmail.com>
2022-10-23 19:07:12 -0600

    remove comments
    Signed-off-by: Annie Li <annielawang@gmail.com>

--- js/script.js
+++ js/script.js
@@ -11,19 +11,6 @@ WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
  */
 // -----------------------------------Variables--------------------------------
-
-// city input
-
-// currentConditions
-// futureConditions
-// cityHistory
-// cityName
-// cityDate
-// cityIcon
-// cityTemp
-// cityHum
-// cityWind
-// searchBtn
 var $searchBtn = $('.btn');
 var $cityName = $('#city');
 var $cardParent = $('#five-days-weather');
